29229fc1c81bb5bba45d419ea2656b8cc033a7c8
Phreakester <gettyv05@gmail.com>
2021-12-01 15:50:52 -0800

    Removed unused classes as only trying to get actuator and radio to work at the moment

--- include/Battery.h
+++
@@ -1,8 +0,0 @@
-class Battery {
-    double voltage;
-    double lastVoltage; //May make this an array to store past 5 voltages to see discharge rate
-    public:
-        Battery(int pin);
-        double measureVoltage() {return voltage;}
-        double getLastVoltage() {return lastVoltage;}
-};
