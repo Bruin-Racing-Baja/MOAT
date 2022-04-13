0fbc28e881642866121ea6376ce7757c25bbc63e
Getty George V <gettyv05@gmail.com>
2022-04-12 21:17:23 -0700

    Developed a lot of useful functions for use in the auto_graphing script

---
+++ data_analysis/analysis_functions.py
@@ -0,0 +1,74 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def create_data_dict(file_path, available_properties):
+    """
+    Creates a dictionary of data from a file path and array of available properties
+    Will return the data dictionary as well as an array containing all properties not available
+    """
+    with open(file_path) as f:
+        data_order = f.readlines()[4].rstrip().split(', ')
+
+
+    raw_data = np.loadtxt(file_path, dtype=float, delimiter=', ', skiprows=5, ndmin=2)
+
+    data = {}
+    not_available = []
+    for i, property in enumerate(data_order):
+        if property in available_properties:
+            data[property] = raw_data[:,i]
+        else:
+            not_available.append(property)
+    return data, not_available
+
+def normalize_data(data, to_normalize):
+    """
+    Normalizes and returns a data dictionary when passed a data dict and an array of properties to normalize
+    """
+    for property in to_normalize:
+        data[property] = (data[property] - np.mean(data[property])) / np.std(data[property])
+    return data
+
+def auto_crop(data_set, leading_indices = 0, following_indices = 0):
+    """
+    Automatically crops the data to the first and last change in the given data set, returning a tuple of the beginning and ending indexes
+    """
+    first_val = data_set[0]
+    last_val = data_set[-1]
+    for i, value in enumerate(data_set):
+        if value != first_val and i >= leading_indices:
+            first_index = i
+            break
+
+    for i, value in enumerate(data_set[::-1]):
+        if value != last_val:
+            last_index = len(data_set) - i
+            try:
+                data_set[last_index + following_indices]
+                last_index += following_indices
+            except IndexError:
+                print("Following indeces are out of bounds")
+            break
+    
+    return (first_index, last_index)
+
+def create_graph(data, x_axis, title = None, cropping = None, file_path = None, *y_axis):
+    """
+    Creates a graph of the given data, as well as however many y_axis are given
+    If a file path is given, the graph will be saved to that file
+    """
+    f = plt.figure()
+    for y_axis_data in y_axis:
+        plt.plot(x_axis[cropping[0]:cropping[1]], y_axis_data[cropping[0]:cropping[1]], label=y_axis_data)
+    plt.xlabel(x_axis)
+    plt.ylabel(y_axis[0])
+    plt.title(title)
+    plt.legend()
+
+    if file_path:
+        f.savefig(file_path)
+        
+    return f
+
+# if program.name == '__main__':
+#     print("This is a module, what are you doing")
--- data_analysis/auto_graph.py
+++ data_analysis/auto_graph.py
@@ -3,15 +3,23 @@
 # are saved in a different folder in the directory
 
 import os
+import numpy as np
+# import matplotlib.pyplot as plt
+from analysis_functions import *
+
+test_status = [2, 4, 4, 4, 4, 4, 4, 4, 4, 5, 8, 4, 7, 9, 4, 4, 4, 4, 4, 4, 4]
 
 # Settings
 target_directory = 'Data_Hopper'
 run_continuously = True
 
-# Main Loop
-while run_continuously:
-    available_files = os.listdir(target_directory)
-    available_files.remove('Output') # Ignore output file
-    current_file = available_files[0] # Select first file in list, doesn't matter the order
-    print('Processing file: ' + current_file)
+
+
+# # Main Loop
+# while run_continuously:
+#     available_files = os.listdir(target_directory)
+#     available_files.remove('Output') # Ignore output file
+#     current_file = available_files[0] # Select first file in list, doesn't matter the order
+#     print('Processing file: ' + current_file)
     
+
