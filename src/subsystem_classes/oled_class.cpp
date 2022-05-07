f1a0c023bd09e400c460e930570d1d5db9dd95d0
Paige <larsonpel181@gmail.com>
2022-05-07 10:43:00 -0700

    most recent oled code (note: still waiting on constants list, map is currently holding random values)

--- src/subsystem_classes/oled_class.cpp
+++ src/subsystem_classes/oled_class.cpp
@@ -1,49 +1,151 @@
 #include <Oled.h>
+#include <EduIntro.h>
 #include <Wire.h>
 #include <Arduino.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <SPI.h>
+Button up(5);
+Button down(9);
+Button cen(7);
+Button plus(8);
+Button minu(6);
 
-void Oled::init(std::map<String,int> ok)
+Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire1, -1);
+std::map<String,int> constants;
+std::map<String,int>::iterator it=constants.begin();
+byte place=1;
+bool onstate;
+
+void Oled::init()
 {
-    Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
-
   // initialize OLED display with address 0x3C for 128x64
-    if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-        Serial.println(F("SSD1306 initialization failed"));
-        while (true);
-    }
+    oled.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+    //oled.setTextSize(1);
+    //oled.setTextColor(WHITE);  
+    oled.clearDisplay();
+    oled.println("TESTING INIT");
+    oled.display();
+    onstate=true;
 
-    oled.clearDisplay(); // clear display
-
-    oled.setTextSize(1);          // text size
-    oled.setTextColor(WHITE);     // text color
-    oled.setCursor(0, 10);        // position to display
-    oled.println("Baja Electronics!"); // text to display
+    //oled.clearDisplay();
+};
+void Oled::write(std::map<String,int> ok){
+    int cursor=10;
+    oled.setCursor(cursor, 0);
+    oled.println("Baja Electronics!");  
+    cursor=cursor+10;       
     for(auto i = ok.cbegin(); i != ok.cend(); ++i){
+      oled.setCursor(cursor,0);
       oled.println(i->first + " " + i->second + "\n");
+      oled.setCursor(60,cursor);
+      cursor=cursor+10;
     }
-    oled.display();               // show on OLED
-};
-void Oled::moveUp(){
 
-};
-void Oled::moveDown(){
+    oled.setCursor(2,(place*10)+10);
+    oled.println(">");
 
-};
-void Oled::increase(){
+    oled.display();  
+}
 
+void Oled::moveUpOrDown(){
+  if(up.pressed()){
+    if(place<=1){
+      place=constants.size()-1;
+    }
+    else{
+      place--;
+    }
+  }
+  if(down.pressed()){
+    if(place>=constants.size()-1){
+      place=1;
+    }
+    else{
+      place++;
+    }
+  }
 };
-void Oled::decrease(){
 
+void Oled::change(std::map<String,int> ok){
+  if(plus.pressed()){
+    switch(place){
+      case 1:
+        it=ok.find(1);
+        if(it!=ok.end()){
+          it->second+=1;
+        }
+        break;
+      case 2:
+        it=ok.find(2);
+        if(it!=ok.end()){
+          it->second+=1;
+        }
+        break;
+      default:
+        oled.print("FAILURE");
+    }
+  }
+  if(minu.pressed()){
+    switch(place){
+      case 1:
+        it=ok.find(1);
+        if(it!=ok.end()){
+          it->second-=1;
+        }
+        break;
+      case 2:
+        it=ok.find(2);
+        if(it!=ok.end()){
+          it->second-=1;
+        }
+        break;
+      default:
+        oled.print("FAILURE");
+    }
+  }
+  
 };
-void Oled::off(){
+void Oled::power(){
+  if(cen.pressed()){
+    if(onstate==true){
+      oled.dim(true);
+    }
+    else{
+      oled.dim(false);
+      oled.clearDisplay();
+      oled.display();
+    }
+  }
+  
+};
 
+void Oled::clear(){
+  oled.clearDisplay();
 };
+
 std::map<String,int> Oled::createMap(){
-    std::map<String,int> constants;
-    constants.insert(std::pair<String,int>("Name",24));
+    int a=1;
+    int b=2;
+    int c=3;
+    constants.insert(it,std::pair<String,int>("Testing1",a));
+    constants.insert(it,std::pair<String,int>("Testing2",b));
+    constants.insert(it,std::pair<String,int>("Testing3",c));
+    constants.insert(it,std::pair<String,int>("Name4",27));
+    constants.insert(it,std::pair<String,int>("Name5",28));
+    constants.insert(it,std::pair<String,int>("Name6",29));
+    constants.insert(it,std::pair<String,int>("Name7",30));
+    constants.insert(it,std::pair<String,int>("Name8",31));
+    constants.insert(it,std::pair<String,int>("Name9",32));
+    return constants;
+
 };
+
+
+
+
+
+
 
 
 
